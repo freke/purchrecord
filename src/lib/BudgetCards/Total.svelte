986faa854a66f165fde9b63da92d8d2fed2ba1d5
David Åberg <434309+freke@users.noreply.github.com>
2023-05-01 09:58:14 +0900

    accumulation

--- src/lib/BudgetCards/Total.svelte
+++ src/lib/BudgetCards/Total.svelte
@@ -47,6 +47,9 @@
 
     function sumArray(budget) {
         let sumArray = new Array(12).fill(0);
+        if(budget == null) {
+            return sumArray;
+        }
         const currentBudget = budget.find((b) => b.year === currentYear);
         if (currentBudget) {
             currentBudget.categories.forEach((budget) => {
@@ -59,27 +62,56 @@
     }
 
     function monthTotals(purchases) {
+        if(purchases == null){
+            return [];
+        }
         return Array.from({ length: 12 }, (_, i) => {
             const total = totalsByMonth(purchases).find(
                 (t) => t.month === i + 1 && t.year == currentYear
             );
-            return total ? total.total.toFixed(2) : "0";
+            return total ? total.total : 0;
         });
     }
+
+    function runningSum(nums: number[]): number[] {
+        let sum = 0;
+        return nums.map(num => {
+            sum += num;
+            return sum;
+        });
+    }
+
+    $: month_purchases = monthTotals($purchases);
+    $: month_budget = sumArray($budget);
+    $: accumulation_purchases = runningSum(month_purchases);
+    $: accumulation_budget = runningSum(month_budget);
+    $: month_max = Math.max(...month_purchases, ...month_budget);
+    $: accumulation_max = Math.max(...accumulation_purchases, ...accumulation_budget);
 
     $: options = {
         series: [
             {
                 name: "Expenses",
-                data: $purchases ? monthTotals($purchases) : [],
+                data: month_purchases,
+                type: "bar",
             },
             {
                 name: "Budget",
-                data: $budget ? sumArray($budget) : [],
+                data: month_budget,
+                type: "bar",
+            },
+            {
+                name: "PurchasesAccumulation",
+                data: accumulation_purchases,
+                type: "line",
+            },
+            {
+                name: "BudgetAccumulation",
+                data: accumulation_budget,
+                type: "line",
             },
         ],
         chart: {
-            type: "bar",
             height: 350,
         },
         plotOptions: {
@@ -94,22 +126,47 @@
         },
         stroke: {
             show: true,
-            width: 2,
-            colors: ["transparent"],
+            width: [2,2,3,2],
         },
         xaxis: {
             categories: months,
         },
-        yaxis: {
+        yaxis: [{
+            seriesName: 'Expenses',
+            min: 0,
+            max: month_max,
             title: {
-                text: "¥",
+                text: "Monthly (¥)",
             },
             labels: {
                 formatter: function (value) {
                     return nFormatter(value,2);
                 }
             },
-        },
+        },{
+            seriesName: 'Budget',
+            min: 0,
+            max: month_max,
+            show: false
+        },{
+            seriesName: 'PurchasesAccumulation',
+            min: 0,
+            max: accumulation_max,
+            opposite: true,
+            title: {
+                text: "Accumulation (¥)",
+            },
+            labels: {
+                formatter: function (value) {
+                    return nFormatter(value,2);
+                }
+            },
+        },{
+            seriesName: 'BudgetAccumulation',
+            min: 0,
+            max: accumulation_max,
+            show: false
+        }],
         fill: {
             opacity: 1,
         },
